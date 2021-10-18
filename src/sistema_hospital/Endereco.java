6d0f3d7bd6d6e8acc0bf5b9e0b4ab11890d1a5f1
Daniel G Selistre <89612901+DanielSelistre@users.noreply.github.com>
2021-10-18 20:26:21 -0300

    Add files via upload

---
+++ src/sistema_hospital/Endereco.java
@@ -0,0 +1,63 @@
+package sistema_hospital;
+
+public class Endereco {
+	private String rua;
+	private String numero;
+	private String bairro;
+	private String cidade;
+	private String estado;
+
+	public Endereco(String rua, String numero, String bairro, String cidade, String estado, String cep) {
+		this.rua = rua;
+		this.numero = numero;
+		this.bairro = bairro;
+		this.cidade = cidade;
+		this.estado = estado;
+	}
+
+	public String getRua() {
+		return this.rua;
+	}
+
+	public String getNumero() {
+		return this.numero;
+	}
+
+	public String getBairro() {
+		return this.bairro;
+	}
+
+	public String getCidade() {
+		return this.cidade;
+	}
+
+	public String getEstado() {
+		return this.estado;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+	
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+	
+	
+	public String toString() {
+		return String.format("Logradouro: %s nº %s, bairro %s - %s/%s", this.rua,this.numero,this.bairro,this.cidade,this.estado);
+	}
+	
+}
---
+++ src/sistema_hospital/ListaMedico.java
@@ -0,0 +1,42 @@
+package sistema_hospital;
+
+import java.util.ArrayList;
+
+public class ListaMedico {
+	private ArrayList<Medico> medico = new ArrayList<Medico>();
+
+	public void cadastrar(Medico medico) {
+		if (medico != null) {
+			medico.add(medico);
+		}
+	}
+
+	public void remover(Medico medico) {
+		medico.removeIf((elemento) -> elemento.getCpf().equals(cliente.getCpf()));
+	}
+
+	public Medico buscar(Medico medico) {
+		for (Medico elemento : this.medico) {
+			if (elemento.getCpf().equals(medico.getCpf())) {
+				return elemento;
+			}
+		}
+		return null;
+	}
+
+	public void editar(Medico medico) {
+		int indice = medico.indexOf(this.buscar(medico));
+		if (indice != -1) {
+			medico.set(indice, medico);
+		}
+	}
+
+	public String listar() {
+		StringBuilder builder = new StringBuilder();
+		for (Medico medico : this.medico) {
+			builder.append(medico.toString());
+		}
+		return builder.toString();
+	}
+
+}
